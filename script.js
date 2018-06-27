c8abd334475cc0a962b4fa43e29a8e5df424e771
Amy Zhang <amyzhang117@gmail.com>
2018-06-27 01:14:38 -0400

    redesign 2

--- script.js
+++ script.js
@@ -1,12 +1,74 @@
+var left;
+var opacity;
+var underlines = ['google', 'twitch', 'akamai', 'others', 'home']
+var pastUnderlines = ['twitch', 'akamai', 'others']
+
+if (screen.width < 1024) {
+    left = "5%";
+    opacity = 0.3;
+} else {
+    left = "15%";
+    opacity = 0.4;
+}
+
 $(document).ready(function() {
-	$('#fullpage').fullpage();
+    $('.container').css({opacity: 0, left: "+=10"}).animate({left: left, opacity: 1});
 
-	$(".down-arrow").click(function() {
-  		$.fn.fullpage.moveSectionDown();
-	});
+    underlines.forEach(function(label) {
+        // clicking underline
+        $('.link-' + label).click(function() {
+            if ($('.sub-' + label).css("display") == "none") {
+                expand(label);
+            } else {
+                reset(label);
+            }
+        });
 
-	$(".up-arrow").click(function() {
-  		$.fn.fullpage.moveTo(1);
-	});
-	
+        // click bullet arrow to reset
+        $('.arrow-' + label).click(function() {
+            if ($('.sub-' + label).css("display") != "none") {
+                reset(label);
+            }
+        });
+    });
 });
+
+function expand(item) {
+    // reset other open subs
+    underlines.forEach(function(label) {
+        if (pastUnderlines.includes(item) && !pastUnderlines.includes(label)) {
+            reset(label);
+        } else if (!pastUnderlines.includes(item) && label != item) {
+            reset(label);
+        }
+    });
+
+    $('.arrow-' + item).removeClass('fa-angle-double-right').addClass('fa-angle-up');
+
+    $('.title').css("opacity", opacity);
+    $('.subtitle').css("opacity", opacity);
+    $('li').not('.nofade-' + item).css("opacity", opacity);
+
+    $('.sub-' + item).fadeIn();
+}
+
+function reset(item) {
+    $('.sub-' + item).fadeOut('fast', function() {
+        // callback function
+        // only reset arrow if all subs below are hidden
+        if (pastUnderlines.includes(item)) {
+            var resetArrow = true;
+            pastUnderlines.forEach(function(label) {
+                console.log($('.sub-' + label).css("display"));
+                if ($('.sub-' + label).css("display") != "none") resetArrow = false;
+            });
+            if (resetArrow) $('.arrow-' + item).removeClass('fa-angle-up').addClass('fa-angle-double-right');
+        } else {
+            $('.arrow-' + item).removeClass('fa-angle-up').addClass('fa-angle-double-right');
+        }
+    });
+
+    $('.title').css("opacity", 1);
+    $('.subtitle').css("opacity", 1);
+    $('li').not('.nofade-' + item).css("opacity", 1);
+}
